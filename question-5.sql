2be261aff4123702680b20cc030a1e64acdb6e0b
junemih <junehong@uw.edu>
2023-04-11 14:44:37 -0700

    final changes

--- question-5.sql
+++ question-5.sql
@@ -1,13 +1,18 @@
 -- What was the most purchased track of 2013?
 
-SELECT 
-  tracks.TrackId, 
-  tracks.Name, 
-  COUNT(*) AS total_purchases
+SELECT tracks.TrackId, tracks.Name, COUNT(*) AS occurrence_count
 FROM invoice_items
 JOIN tracks ON invoice_items.TrackId = tracks.TrackId
 JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId
 WHERE strftime('%Y', invoices.InvoiceDate) = '2013'
 GROUP BY invoice_items.TrackId
-ORDER BY COUNT(*) DESC
-LIMIT 1;
+HAVING COUNT(*) = (
+  SELECT COUNT(*) AS highest_count
+  FROM invoice_items
+  JOIN invoices ON invoice_items.InvoiceId = invoices.InvoiceId
+  WHERE strftime('%Y', invoices.InvoiceDate) = '2013'
+  GROUP BY invoice_items.TrackId
+  ORDER BY COUNT(*) DESC
+  LIMIT 1
+)
+ORDER BY tracks.Name ASC;
